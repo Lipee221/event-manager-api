055d9670e5fa56d55f9ef9e10d95baa14ffa01fc
lipe221 <oida9022@gmail.com>
2025-04-11 20:48:18 -0300

    chore: criação de middleware de erro e preparação para validações

--- index.ts
+++ index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import userRoutes from './src/routes/user.router';
 import eventRoutes from './src/routes/event.routes';
 import ticketRoutes from './src/routes/ticket.routes'
-
+import { errorHandler } from './src/middlewares/error.middleware';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+app.use(errorHandler);
 app.use('/tickets', ticketRoutes);
 app.use('/events', eventRoutes);
 app.use(express.json());
